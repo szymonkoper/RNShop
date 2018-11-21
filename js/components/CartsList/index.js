96385c51b4f0b7f93b47a7f162dc5cef0faec6bc
Szymon Koper <sz.koper@gmail.com>
2018-11-21 13:14:07 +0100

    Adds uuid to distinguish unique items in lists

--- js/components/CartsList/index.js
+++ js/components/CartsList/index.js
@@ -5,8 +5,10 @@ const renderItem = ({ item }) => (
   <Text>{item.name}</Text>
 );
 
+const getItemKey = item => item.uuid;
+
 const CartsList = ({ carts }) => (
-  <FlatList data={carts} keyExtractor={item => item.name} renderItem={renderItem} />
+  <FlatList data={carts} keyExtractor={getItemKey} renderItem={renderItem} />
 );
 
 export default CartsList;
--- js/models/Cart.js
+++ js/models/Cart.js
@@ -1,5 +1,8 @@
+import uuid from 'uuid/v4';
+
 export default class Cart {
   constructor(name, modificationDate, items, archived = false) {
+    this.uuid = uuid();
     this.name = name;
     this.modificationDate = modificationDate;
     this.items = items;
