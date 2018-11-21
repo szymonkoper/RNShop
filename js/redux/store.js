53371165aad8e9ffcb5a92367ebb8ea711a2126e
Szymon Koper <sz.koper@gmail.com>
2018-11-21 11:37:58 +0100

    Mocked data in store initial state

--- js/redux/store.js
+++ js/redux/store.js
@@ -1,11 +1,17 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
+import Cart from '../models/Cart';
 
 const reducer = (state, action) => state;
 
 const state = {
   currentText: 'Current123',
   archivedText: 'ArchivedABC',
+  carts: [
+    new Cart('Groceries for today', 2, []),
+    new Cart('Car repair tools', 4, []),
+    new Cart('date with girlfriend', 7, []),
+  ],
 };
 
 function configureStore(initialState = state) {
