46d5816d672f35043c842c13321a0d0a40127ff5
Szymon Koper <sz.koper@gmail.com>
2018-11-21 13:27:24 +0100

    Uses sort-by-chain (BTW: it's the best sorting library in the world) to sort shopping lists by dates and names

--- js/components/CartsList/index.js
+++ js/components/CartsList/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { FlatList, Text } from 'react-native';
+import { sortByChain } from 'sort-by-chain'; // <--- This is my library :)
 
-const renderItem = ({ item }) => (
-  <Text>{item.name}</Text>
-);
+// Simple sorting of shopping lists by modification date should be enough,
+// but I couldn't resist urge to show you my awesome 'sort-by-chain' library. :)
+// It builds a custom comparator from this chain to sort it in a correct order.
+const sortingChain = [
+  { valueGetter: it => it.modificationDate, reverse: true },
+  { valueGetter: it => it.name },
+];
+
+const orderCarts = carts => sortByChain(carts, sortingChain);
 
 const getItemKey = item => item.uuid;
 
+const renderItem = ({ item }) => (
+  <Text>{`${item.modificationDate}: ${item.name}`}</Text>
+);
+
 const CartsList = ({ carts }) => (
-  <FlatList data={carts} keyExtractor={getItemKey} renderItem={renderItem} />
+  <FlatList data={orderCarts(carts)} keyExtractor={getItemKey} renderItem={renderItem} />
 );
 
 export default CartsList;
