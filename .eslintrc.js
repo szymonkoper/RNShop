ee9baacd9e50454e05b5954d588701b4b26f551e
Szymon Koper <sz.koper@gmail.com>
2018-11-21 09:25:25 +0100

    Adds eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "react-native",
+        "jest",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "no-console": 0,
+        "react/jsx-filename-extension": [ 1, { "extensions": [ ".js", ".jsx" ] } ],
+        "react/prop-types": ["error", { "ignore": ["navigation"] }],
+    },
+  };
+  
