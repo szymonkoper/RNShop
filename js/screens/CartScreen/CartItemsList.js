dd41ceccf436eadd09bd565e8e2acf9be7f42cfd
Szymon Koper <sz.koper@gmail.com>
2018-11-22 20:37:48 +0100

    Sorting of items in CartItemsList

--- js/screens/CartScreen/CartItemsList.js
+++ js/screens/CartScreen/CartItemsList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { FlatList, Text } from 'react-native';
+import { sortByChain } from 'sort-by-chain';
 
-// TODO: Order items!
 // TODO: Extract item view
+const sortingChain = [
+  { valueGetter: it => it.modificationDate, reverse: true },
+  { valueGetter: it => it.name },
+];
 
 const CartItemsList = ({ items }) => (
   <FlatList
-    data={items}
+    data={sortByChain(items, sortingChain)}
     keyExtractor={item => item.uuid}
     renderItem={listItem => <Text>{listItem.item.name}</Text>}
   />
