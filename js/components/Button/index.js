e8f4078717ec13ce91885efad102c2485099654e
Szymon Koper <sz.koper@gmail.com>
2018-11-22 22:27:20 +0100

    Updated Button to accept other props

--- js/components/Button/index.js
+++ js/components/Button/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 
-const Button = ({ label, onTap }) => (
-  <TouchableOpacity onPress={onTap}>
+const Button = ({ label, onTap, ...otherProps }) => (
+  <TouchableOpacity onPress={onTap} {...otherProps}>
     <Text>{label}</Text>
   </TouchableOpacity>
 );
