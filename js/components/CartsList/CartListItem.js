ff3ad564ff32a40134a2a3de59bacd1c10af3b3f
Szymon Koper <sz.koper@gmail.com>
2018-11-23 03:17:58 +0100

    Adds propTypes to CartListItem

--- js/components/CartsList/CartListItem.js
+++ js/components/CartsList/CartListItem.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
+import propTypes from 'prop-types';
 
 const CartListItem = ({ item, onItemTap, onItemLongPress }) => (
   <TouchableOpacity onPress={() => onItemTap(item)} onLongPress={() => onItemLongPress(item)}>
     <Text>{`${item.modificationDate}: ${item.name}`}</Text>
   </TouchableOpacity>
 );
+
+CartListItem.propTypes = {
+  item: propTypes.shape({
+    name: propTypes.string,
+    items: propTypes.array,
+  }).isRequired,
+  onItemTap: propTypes.func.isRequired,
+  onItemLongPress: propTypes.func.isRequired,
+};
 
 export default CartListItem;
