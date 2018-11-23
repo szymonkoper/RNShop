1239b95efbfb1282992a049bccfacf5ac0d44822
Szymon Koper <sz.koper@gmail.com>
2018-11-23 03:33:28 +0100

    Styled CartListItem

--- js/components/CartsList/CartListItem.js
+++ js/components/CartsList/CartListItem.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
 import propTypes from 'prop-types';
 
+const styles = StyleSheet.create({
+  layout: {
+    borderRadius: 5,
+    backgroundColor: '#016FB9',
+    padding: 15,
+    margin: 10,
+    flexDirection: 'row',
+  },
+  textName: {
+    color: '#ffffff',
+    flex: 1,
+    fontSize: 16,
+  },
+  textCount: {
+    color: '#ffffff',
+    fontSize: 18,
+  },
+});
+
 const CartListItem = ({ item, onItemTap, onItemLongPress }) => (
-  <TouchableOpacity onPress={() => onItemTap(item)} onLongPress={() => onItemLongPress(item)}>
-    <Text>{`${item.modificationDate}: ${item.name}`}</Text>
+  <TouchableOpacity
+    style={styles.layout}
+    onPress={() => onItemTap(item)}
+    onLongPress={() => onItemLongPress(item)}
+  >
+    <Text style={styles.textName} numberOfLines={1}>{item.name}</Text>
+    <Text style={styles.textCount}>{item.items.length}</Text>
   </TouchableOpacity>
 );
 
